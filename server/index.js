c52c9fe8cd9859ac36c2b1ef95f5dff8c57827da
Arpit Kumar <164194769+arpitk545@users.noreply.github.com>
2026-04-25 12:33:45 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const dotenv = require('dotenv');
+const cors = require('cors');
 const database =require('./config/database');
 const authRoutes =require('./routes/Auth');
 const attendanceRoutes =require("./routes/attendance");
@@ -17,6 +18,18 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 4000;
 dotenv.config();
 database.connect();
+
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://gecaiml.vercel.app",
+];
+
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true,
+  methods: ["GET", "POST", "PUT","PATCH", "DELETE"],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use("/api/v1/auth",authRoutes)
 app.use("/api/v1/attendance",attendanceRoutes);
